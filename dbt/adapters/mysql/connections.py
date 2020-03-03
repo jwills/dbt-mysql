00e30ca074a3449101e958ce931cdb06217907f9
Josh Wills <jwills@apache.org>
2020-03-03 13:14:13 -0800

    wip, need to figure out cancellation model

--- dbt/adapters/mysql/connections.py
+++ dbt/adapters/mysql/connections.py
@@ -1,7 +1,12 @@
 from contextlib import contextmanager
 
+import MySQLDb
+
+import dbt.compat
+import dbt.exceptions
 from dbt.adapters.base import Credentials
 from dbt.adapters.sql import SQLConnectionManager
+from dbt.logger import GLOBAL_LOGGER as logger
 
 
 MYSQL_CREDENTIALS_CONTRACT = {
@@ -32,6 +37,10 @@ MYSQL_CREDENTIALS_CONTRACT = {
 
 class MySQLCredentials(Credentials):
     SCHEMA = MYSQL_CREDENTIALS_CONTRACT
+    ALIASES = {
+      'db': 'database',
+      'passwd': 'password',
+    }
 
     @property
     def type(self):
@@ -43,3 +52,56 @@ class MySQLCredentials(Credentials):
 
 class MySQLConnectionManager(SQLConnectionManager):
     TYPE = 'mysql'
+
+    @classmethod
+    def open(cls, connection):
+        if connection.state == 'open':
+            logger.debug('Connection is already open, skipping open.')
+            return connection
+
+        base_credentials = connection.credentials
+        credentials = cls.get_credentials(connection.credentials.incorporate())
+
+        try:
+            handle = MySQLdb.connect(
+                dbname=credentials.database,
+                user=credentials.user,
+                host=credentials.host,
+                password=credentials.password,
+                port=credentials.port,
+                connect_timeout=10)
+            
+            connection.handle = handle
+            connection.state = 'open'
+        except MySQLdb.Error as e:
+            logger.debug("Got an error when attempting to open a postgres "
+                         "connection: '{}'"
+                         .format(e))
+
+            connection.handle = None
+            connection.state = 'fail'
+
+            raise dbt.exceptions.FailedToConnectException(str(e))
+
+        return connection
+
+    def cancel(self, connection):
+        connection_name = connection.name
+        pid = connection.handle.get_backend_pid()
+
+        sql = "select pg_terminate_backend({})".format(pid)
+
+        logger.debug("Cancelling query '{}' ({})".format(connection_name, pid))
+
+        _, cursor = self.add_query(sql, 'master')
+        res = cursor.fetchone()
+
+        logger.debug("Cancel query '{}': {}".format(connection_name, res))
+
+    @classmethod
+    def get_credentials(cls, credentials):
+        return credentials
+
+    @classmethod
+    def get_status(cls, cursor):
+        return cursor.statusmessage
